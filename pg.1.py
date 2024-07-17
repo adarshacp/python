9a771f4131189e6fd1cb095b7ebd3f3a757feee1
adarshacp <adarshacp53@gmail.com>
2024-07-17 17:35:44 +0530

    first-commit

---
+++ pg.1.py
@@ -0,0 +1,33 @@
+number1=100
+print("this type of number1 is:",type(number1))
+integer_1=100
+integer_2=200
+
+print(integer_1*integer_2)
+print(integer_1+integer_2)
+print(integer_1-integer_2)
+print(integer_1/integer_2)
+
+number2=100.0
+print("this type of number1 is:",type(number2))
+float_1=12.539
+float_2=6.78
+print(float_1*float_2)
+print(float_1+float_2)
+print(float_1-float_2)
+print(float_1/float_2)
+
+str="jack Danies"
+print("this type of string is:",type(str))
+str_1="hello"
+str_2="world"
+print(str_1+str_2)
+
+result=100<200
+print("this type of result is:",type(result))
+has_passed=False
+marks=80
+if marks>50:
+    has_passed=True
+print(has_passed)
+
